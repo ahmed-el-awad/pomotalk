01fb0949ddd85b82506e3d366a71f3ae6afe3586
Ahmed <73728149+ahmed-el-awad@users.noreply.github.com>
2025-05-26 19:01:06 +0400

    refactor: remove clock.js

--- clock.js
+++
@@ -1,8 +0,0 @@
-import { getHours, getMinutes, getSeconds } from "date-fns";
-const today = new Date();
-
-const h = getHours(today);
-const m = getMinutes(today);
-const s = getSeconds(today);
-
-console.log(`${h}:${m}:${s}`);
