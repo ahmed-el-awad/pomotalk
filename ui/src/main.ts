f73e18d527baa7d20813cb9cbf78da78693cad9c
Ahmed <73728149+ahmed-el-awad@users.noreply.github.com>
2025-05-26 18:19:46 +0400

    refactor(ui): replace vite files with project files

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,24 +1,90 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const rejectedSymbols = ["-", "+", "e", "E", "."];
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+function setMinutesValue(val) {
+  document.querySelector("#minutes").value = val;
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function setSecondsValue(val) {
+  document.querySelector("#seconds").value = val;
+}
+
+function validateMinutesTime(minHTML) {
+  const minutes = minHTML.value;
+
+  // remove all rejected symbols from the string
+  const filteredValue = minutes
+    .split("")
+    .filter((char) => !rejectedSymbols.includes(char))
+    .join("");
+
+  console.log(filteredValue);
+
+  if (filteredValue.length > 2) {
+    setMinutesValue(filteredValue.slice(0, -1));
+  } else {
+    setMinutesValue(filteredValue);
+  }
+}
+
+function validateSecondsTime(secHTML) {
+  const seconds = secHTML.value;
+
+  // remove all rejected symbols from the string
+  const filteredValue = seconds
+    .split("")
+    .filter((char) => !rejectedSymbols.includes(char))
+    .join("");
+
+  console.log(filteredValue);
+
+  if (filteredValue.length > 2) {
+    setSecondsValue(filteredValue.slice(0, -1));
+  } else {
+    setSecondsValue(filteredValue);
+  }
+}
+
+function makeMinutesZero(minHTML) {
+  const minutes = minHTML.value;
+
+  if (minutes.length == 0) {
+    setMinutesValue(0);
+  }
+
+  // if there's multiple 0s, just have a single 0
+  if (minutes.split("").every((n) => n == "0")) {
+    setMinutesValue(0);
+  }
+
+  if (parseInt(minutes) > 59) {
+    setMinutesValue(59);
+  }
+}
+
+function makeSecondsZero(secHTML) {
+  const seconds = secHTML.value;
+
+  if (seconds.length == 0) {
+    setSecondsValue(0);
+  }
+
+  // if there's multiple 0s, just have a single 0
+  if (seconds.split("").every((n) => n == "0")) {
+    setSecondsValue(0);
+  }
+
+  if (parseInt(seconds) > 59) {
+    setSecondsValue(59);
+  }
+}
+
+function startCountdown() {
+  let minutes = parseInt(document.querySelector("#minutes").value);
+  let seconds = parseInt(document.querySelector("#seconds").value);
+
+  setInterval(() => {
+    document.querySelector("#time").innerHTML = minutes + ":" + seconds;
+
+    console.log(minutes + ":" + seconds);
+  }, 1000);
+}
