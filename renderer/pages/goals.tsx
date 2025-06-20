e7b5169c565e6bc06c3ee1d9699bf527715d0e7b
Ahmed <73728149+ahmed-el-awad@users.noreply.github.com>
2025-06-20 21:52:04 +0400

    fix: overflow of kanbans
    appears whenever there's a bunch of them

--- renderer/pages/goals.tsx
+++ renderer/pages/goals.tsx
@@ -4,7 +4,7 @@ export default function Goals() {
   return (
     <>
       <div className="mb-4 w-full text-xl font-bold">Goals</div>
-      <div className="flex gap-10 bg-red-300 p-4">
+      <div className="flex gap-10 overflow-x-auto bg-red-300 p-4">
         <Kanban />
         <Kanban />
         <Kanban />
