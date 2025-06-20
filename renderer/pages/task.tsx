7ff5e566c4dc3a722c375760f36d521189314540
Ahmed <73728149+ahmed-el-awad@users.noreply.github.com>
2025-06-20 21:11:41 +0400

    feat: tasks reordering and checkbox

--- renderer/pages/task.tsx
+++ renderer/pages/task.tsx
@@ -1,8 +1,44 @@
-export default function Task() {
+import { Checkbox } from "@heroui/react";
+import { Reorder, useDragControls } from "framer-motion";
+
+interface TaskProps {
+  task: string;
+}
+
+export default function Task({ task }: TaskProps) {
+  const dragControls = useDragControls();
+
   return (
     <>
-      <div className="mb-4 bg-white p-4">
-        <h1>Read 20 pages</h1>
+      <div className="mb-2">
+        <Reorder.Item
+          value={task}
+          dragListener={false}
+          dragControls={dragControls}
+          className="flex justify-between bg-white"
+        >
+          <div className="">
+            <Checkbox lineThrough disableAnimation>
+              <span>{task}</span>
+            </Checkbox>
+          </div>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="reorder-handle ml-4 size-6 cursor-pointer bg-red-400"
+            onPointerDown={(e) => dragControls.start(e)}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
+            />
+          </svg>
+        </Reorder.Item>
       </div>
     </>
   );
--- renderer/pages/tasks.tsx
+++ renderer/pages/tasks.tsx
@@ -1,12 +1,22 @@
+import { Reorder, useDragControls } from "framer-motion";
 import Task from "./task";
+import { useState } from "react";
 
 export default function Tasks() {
+  const [tasks, setTasks] = useState<string[]>([
+    "Read 5 pages",
+    "Read 10 pages",
+    "Read 20 pages",
+  ]);
+
   return (
     <>
-      <div className="flex flex-col">
-        <Task />
-        <Task />
-        <Task />
+      <div className="flex flex-col bg-blue-300 p-2">
+        <Reorder.Group axis="y" values={tasks} onReorder={setTasks}>
+          {tasks.map((task) => (
+            <Task task={task} key={task} />
+          ))}
+        </Reorder.Group>
       </div>
     </>
   );
