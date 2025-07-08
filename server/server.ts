940f381ce28211f1468c4ca423bda939254fd44b
Ahmed <73728149+ahmed-el-awad@users.noreply.github.com>
2025-07-08 22:58:01 +0400

    feat(server): sending messages between clients
    messages sent are stored on the server, they can be retrieved by sending a new message

--- server/server.ts
+++ server/server.ts
@@ -3,12 +3,18 @@ import express from "express";
 import http from "http";
 import { randomUUID } from "crypto";
 
+interface ChatMessage {
+  name: string;
+  message: string;
+}
+
 const port = 8080;
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
 const clients = new Map<string, WebSocket>();
+const messageList: ChatMessage[] = [];
 
 wss.on("connection", (ws: WebSocket) => {
   const clientID = randomUUID().slice(0, 8);
@@ -16,20 +22,35 @@ wss.on("connection", (ws: WebSocket) => {
 
   console.log(`Client ${clientID} connected`);
 
+  // TODO: currently the param is type string but there's no guarantee that it is a string
+  // find out what type is more suitable to be used here
   ws.on("message", (message: string) => {
+    message = message.toString();
     console.log(`Received: ${message} sent by ${clientID}`);
+
+    messageList.push({
+      name: clientID,
+      message: message,
+    });
+
+    // For debugging
+    console.log("current messages");
+    for (const m of messageList) {
+      console.log(m);
+    }
 
     // broadcasts message to every connected client, minus the one sending the message
     clients.forEach((client: WebSocket, currentClientID: string) => {
-      if (currentClientID !== clientID) {
-        client.send(`client #${clientID}: ${message}`);
+      if (client.readyState === WebSocket.OPEN) {
+        // client.send(`client #${clientID}: ${message}`);
+        client.send(JSON.stringify(messageList));
       }
     });
   });
 
   ws.on("close", () => {
     clients.delete(clientID);
-    console.log("Client disconnected");
+    console.log(`Client ${clientID} disconnected`);
   });
 });
 
