0eed960841ca63c55f712bdc3c877fd6dcc38b07
Ahmed <73728149+ahmed-el-awad@users.noreply.github.com>
2025-07-08 23:00:46 +0400

    feat(client): connecting and sending messages to the server

--- renderer/components/chat.tsx
+++ renderer/components/chat.tsx
@@ -1,23 +1,69 @@
 import { Button, Form, Textarea } from "@heroui/react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+interface ChatMessage {
+  name: string;
+  message: string;
+}
 
 export default function Chat() {
+  const [chatMessages, setChatMessages] = useState<ChatMessage[]>([]);
   const [message, setMessage] = useState("");
+  const socketRef = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    socketRef.current = new WebSocket("ws://localhost:8080");
+
+    socketRef.current.onopen = () => {
+      console.log("Websocket connected");
+    };
+
+    socketRef.current.onmessage = (event) => {
+      const newMessagesFromServer: ChatMessage[] = JSON.parse(event.data);
+      console.log("Message from server:", newMessagesFromServer);
+
+      setChatMessages(newMessagesFromServer);
+    };
+
+    socketRef.current.onclose = () => {
+      console.error("WebSocket connection was lost");
+    };
+
+    return () => socketRef.current.close();
+  }, []);
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    console.log(
-      "message:",
-      (e.currentTarget.elements.namedItem("message") as HTMLInputElement).value
-    );
+    if (socketRef.current?.readyState === WebSocket.OPEN) {
+      socketRef.current.send(message);
+      console.log("sent message", message);
 
-    setMessage("");
+      setMessage("");
+    } else {
+      console.log("websocket is not connected");
+      console.log("trying to reconnect...");
+
+      socketRef.current = new WebSocket("ws://localhost:8080");
+
+      socketRef.current.onopen = () => {
+        console.log("Websocket connected");
+      };
+    }
   };
 
   return (
     <div className="col-span-2 flex flex-col justify-between bg-blue-300">
       <h1 className="ml-2 mt-2 text-xl font-bold">Chat</h1>
+      <div>
+        {chatMessages.map((messageInfo, idx) => {
+          return (
+            <div key={idx} className="mx-4 bg-red-400 px-4">
+              {messageInfo.message}
+            </div>
+          );
+        })}
+      </div>
       <Form onSubmit={onSubmit} className="flex flex-row items-center">
         <Textarea
           name="message"
