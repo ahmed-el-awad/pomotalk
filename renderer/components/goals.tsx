1a121e3f3865a8bec201dc4cd1c1cb02236171dd
Ahmed Elawad <73728149+ahmed-el-awad@users.noreply.github.com>
2026-05-02 14:55:57 +0400

    refactor: goals component

--- renderer/components/goals.tsx
+++ renderer/components/goals.tsx
@@ -3,9 +3,11 @@ import Kanban from "./kanban";
 export default function Goals() {
   return (
     <>
-      <div className="mt-4 text-xl font-bold">
-        <h1>Goals</h1>
-        <div className="flex gap-10 overflow-x-auto overflow-y-hidden bg-red-300 p-4">
+      <div className="mt-2 text-xl font-bold">
+        <div className="bg-temp-5 flex overflow-x-auto rounded-lg pt-4">
+          <Kanban />
+          <Kanban />
+          <Kanban />
           <Kanban />
           <Kanban />
           <Kanban />
