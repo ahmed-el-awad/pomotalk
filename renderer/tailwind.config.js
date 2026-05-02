af17148b40b1ec6a20daa29eece4e96abcce5a3a
Ahmed Elawad <73728149+ahmed-el-awad@users.noreply.github.com>
2026-05-02 14:54:04 +0400

    feat: tailwind temporary colors

--- renderer/tailwind.config.js
+++ renderer/tailwind.config.js
@@ -10,6 +10,13 @@ module.exports = {
   theme: {
     colors: {
       ...colors,
+      temp: {
+        1: colors.slate["100"],
+        2: colors.slate["200"],
+        3: colors.slate["300"],
+        4: colors.slate["400"],
+        5: colors.slate["500"],
+      },
     },
     extend: {},
   },
